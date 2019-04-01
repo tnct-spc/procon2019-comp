ea510187672f6baac78ff9f6a7b081ee375bbbc9
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-01 19:11:29 +0900

    [update]Point

--- Utilities/utils.cpp
+++ Utilities/utils.cpp
@@ -1,6 +1,29 @@
 #include "utils.h"
 
+namespace procon{
 
-procon::Point::Point()
-{
+Point& Point::operator+=(const Point& p){
+    x += p.x;
+    y += p.y;
+    return *this;
+}
+
+Point& Point::operator-=(const Point& p){
+    x -= p.x;
+    y -= p.y;
+    return *this;
+}
+
+bool operator==(const Point& p, const Point& q){
+    return p.x == q.x && p.y == q.y;
+}
+
+bool operator<(const Point& p, const Point& q){
+    return p.x < q.x || (p.x == q.x && p.y < q.y);
+}
+
+constexpr Point Point::getMove(int move_index){
+    return Point(dx[move_index], dy[move_index]);
+}
+
 }
--- Utilities/utils.h
+++ Utilities/utils.h
@@ -5,14 +5,34 @@
 
 namespace procon{
 
-const std::vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1, 0};
-const std::vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1, 0};
+// constexprのために生配列を使う (C++20ならstd::vectorがconstexprで使える)
+constexpr int dx[9] = {-1, -1, -1, 0, 0, 1, 1, 1, 0};
+constexpr int dy[9] = {-1, 0, 1, -1, 1, -1, 0, 1, 0};
 
-class Point
+struct Point
 {
-public:
-    Point();
+    constexpr Point() : x(0), y(0){}
+    constexpr Point(int x, int y) : x(x), y(y){}
+
+    Point& operator+=(const Point& p);
+    Point& operator-=(const Point& p);
+
+    friend Point operator+(const Point& p, const Point& q){return Point(p) += q;}
+    friend Point operator-(const Point& p, const Point& q){return Point(p) -= q;}
+
+    friend bool operator==(const Point& p, const Point& q);
+    friend bool operator!=(const Point& p, const Point& q){return !(p == q);}
+
+    friend bool operator<(const Point& p, const Point& q);
+    friend bool operator<=(const Point& p, const Point& q){return p == q || p < q;}
+    friend bool operator>(const Point& p, const Point& q){return !(p <= q);}
+    friend bool operator>=(const Point& p, const Point& q){return !(p < q);}
+
+    constexpr Point getMove(int move_index);
+
+    int x, y;
 };
+
 
 struct MoveState{
 };
