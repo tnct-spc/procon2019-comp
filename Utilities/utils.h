5e75c281af6a177179b3f2fc931bfbceb83d81f3
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-01 18:43:13 +0900

    [add]dx, dy

--- Utilities/utils.h
+++ Utilities/utils.h
@@ -1,7 +1,12 @@
 #ifndef STATES_H
 #define STATES_H
 
+#include <vector>
+
 namespace procon{
+
+const std::vector<int> dx = {-1, -1, -1, 0, 0, 1, 1, 1, 0};
+const std::vector<int> dy = {-1, 0, 1, -1, 1, -1, 0, 1, 0};
 
 class Point
 {
