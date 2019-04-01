767f7bb7a7af303c7a1628ef0181f9dd01c5f3e6
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-02 00:33:38 +0900

    [add]Field constructor, converter, getState

--- Utilities/field.cpp
+++ Utilities/field.cpp
@@ -1,6 +1,22 @@
 #include "field.h"
 
+namespace procon{
 
-procon::Field::Field()
+constexpr procon::Field::Field(Point size) :
+    size(size)
 {
+}
+
+constexpr procon::Field::Field(int size_x, int size_y) :
+    size(size_x, size_y)
+{
+}
+
+
+constexpr int Field::getState(Point p) const{
+    assert(0 <= p.x && p.x < size.x && 0 <= p.y && p.y < size.y);
+    auto shift_count = pointToInt(p) * 2;
+    return (data[shift_count + 1] << 1) + data[shift_count];
+}
+
 }
--- Utilities/field.h
+++ Utilities/field.h
@@ -1,12 +1,28 @@
 #ifndef FIELD_H
 #define FIELD_H
 
+#include <bitset>
+#include <cassert>
+#include "utils.h"
+
 namespace procon{
 
 class Field
 {
 public:
-    Field();
+    constexpr Field(Point size = Point(10, 10));
+    constexpr Field(int size_x, int size_y);
+
+    constexpr int getState(Point p) const;
+    constexpr int getState(int x, int y) const{return getState(Point(x, y));}
+
+    constexpr int pointToInt(const Point& p) const{return p.x * size.y + p.y;}
+    constexpr Point IntToPoint(const int i) const{return Point(i / size.y, i % size.y);}
+
+private:
+
+    Point size;
+    std::bitset<800> data;
 };
 
 }
