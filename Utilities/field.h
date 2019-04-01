b9494f06c3bb7a450138ecd22995edb95327e273
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-02 00:50:39 +0900

    [add]agent getter/setter

--- Utilities/field.h
+++ Utilities/field.h
@@ -17,6 +17,9 @@ public:
     constexpr int getState(Point p) const;
     constexpr int getState(int x, int y) const{return getState(Point(x, y));}
 
+    constexpr Point getAgent(bool team, int agent_index){return agents.at(team).at(agent_index);}
+    constexpr void setAgent(bool team, int agent_index, Point agent_data){agents.at(team).at(agent_index) = agent_data;}
+
     constexpr int pointToInt(const Point& p) const{return p.x * size.y + p.y;}
     constexpr Point IntToPoint(const int i) const{return Point(i / size.y, i % size.y);}
 
