1d7adf60f91c7ce643cd4153be42e8fb6dfa7a82
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-04 08:23:28 +0900

    [fix]algo init timing

--- GameManager/gamemanager.cpp
+++ GameManager/gamemanager.cpp
@@ -5,9 +5,6 @@ GameManager::GameManager() :
     algo(2)
 {
     visualizer.show();
-    // 仮実装という事で、algoにランダムウォーク2つを入れて、Visualizerがクリックされる毎に更新を行うようなものを考える
-    algo.at(0) = std::make_shared<TestAlgorithm>(*field, 0);
-    algo.at(1) = std::make_shared<TestAlgorithm>(*field, 1);
 }
 
 void GameManager::runSimulation(){
@@ -15,4 +12,8 @@ void GameManager::runSimulation(){
     game = std::make_shared<GameSimulator>();
     field = game->getFieldPtr();
     visualizer.setFieldPtr(field);
+
+    // 仮実装という事で、algoにランダムウォーク2つを入れて、Visualizerがクリックされる毎に更新を行うようなものを考える
+    algo.at(0) = std::make_shared<TestAlgorithm>(*field, 0);
+    algo.at(1) = std::make_shared<TestAlgorithm>(*field, 1);
 }
