e993d03b91f75f2d487844e736f310133e5b9ab3
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-03 22:30:51 +0900

    [add]Visualizer draw functions

--- Visualizer/main.cpp
+++ Visualizer/main.cpp
@@ -1,10 +1,13 @@
 #include "visualizer.h"
 #include <QApplication>
 
+#include "field.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Visualizer w;
+    auto f = procon::Field::generateRandomField();
+    Visualizer w(f);
     w.show();
 
     return a.exec();
