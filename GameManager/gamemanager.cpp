44af237fe825135565f5fd979452b0c80b056a7a
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-04 09:09:57 +0900

    [update]runSimulation playout

--- GameManager/gamemanager.cpp
+++ GameManager/gamemanager.cpp
@@ -17,5 +17,9 @@ void GameManager::runSimulation(){
     algo.at(0) = std::make_shared<TestAlgorithm>(*field, 0);
     algo.at(1) = std::make_shared<TestAlgorithm>(*field, 1);
 
-    game->turnSimulation(algo.at(0), algo.at(1));
+    while(!game->isSimulationEnded()){
+        game->turnSimulation(algo.at(0), algo.at(1));
+        visualizer.update();
+        visualizer.repaint();
+    }
 }
