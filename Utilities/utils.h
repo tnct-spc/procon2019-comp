1bfbb391e9ecef4d697e6fef5db083133f7ec905
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-05 22:57:34 +0900

    [add]calcRegionPoint

--- Utilities/utils.h
+++ Utilities/utils.h
@@ -8,8 +8,8 @@
 
 namespace procon{
 
-constexpr std::array<int, 9> dx = {-1, -1, -1, 0, 0, 1, 1, 1, 0};
-constexpr std::array<int, 9> dy = {-1, 0, 1, -1, 1, -1, 0, 1, 0};
+constexpr std::array<int, 9> dx = {-1, 1, 0, 0, -1, 1, -1, 1, 0};
+constexpr std::array<int, 9> dy = {0, 0, 1, -1, 1, -1, -1, 1, 0};
 
 struct MoveState;
 
