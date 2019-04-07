71dc22912803d99a60cc6741408140d0c6cf1e12
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-07 20:20:24 +0900

    [change]TestALgorithm::agentAct return value

--- Algorithms/testalgorithm.cpp
+++ Algorithms/testalgorithm.cpp
@@ -1,6 +1,5 @@
 #include "testalgorithm.h"
 
 std::vector<procon::MoveState> TestAlgorithm::agentAct(){
-    // ランダムウォークで移動のみを行う(タイル削除は行わない)
-    return std::vector<procon::MoveState>(field.getAgentCount(), procon::MoveState(procon::random::call(8), false));
+    return std::vector<procon::MoveState>(field.getAgentCount());
 }
