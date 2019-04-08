11e39b76ba5e078bdb132c2dfc4f45e8314d7577
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-08 15:41:10 +0900

    [add]testMakeConflict

--- Algorithms/testalgorithm.cpp
+++ Algorithms/testalgorithm.cpp
@@ -1,5 +1,108 @@
 #include "testalgorithm.h"
 
 std::vector<procon::MoveState> TestAlgorithm::agentAct(){
-    return std::vector<procon::MoveState>(field.getAgentCount());
+
+    return testMakeConflict();
+
+    // return std::vector<procon::MoveState>(field.getAgentCount());
+}
+
+std::vector<procon::MoveState> TestAlgorithm::testMakeConflict(){
+
+    SimpleBeamSearch enemy(field, !side);
+    std::vector<procon::MoveState> moves(enemy.agentAct());
+
+    const auto& size = field.getSize();
+    int agent_count = field.getAgentCount();
+
+    std::set<procon::Point> enemy_agent_points, enemy_conflict_points;
+
+    // 相手が移動を選んだ場合(タイルの置かれていないマスに移動とした場合)、コンフリクトに回せる場合はそれをします
+    for(int agent_index = 0; agent_index < agent_count; ++agent_index){
+        auto before_pos = field.getAgent(!side, agent_index);
+        auto after_pos = before_pos.getAppliedPosition(moves.at(agent_index).move_index);
+        enemy_agent_points.emplace(before_pos);
+        enemy_conflict_points.emplace(after_pos);
+    }
+
+    std::vector<std::vector<int>> tile_scores(size.x, std::vector<int>(size.y, 0));
+    for(int x_index = 0; x_index < size.x; ++x_index)
+        for(int y_index = 0; y_index < size.y; ++y_index){
+            if(enemy_agent_points.find(procon::Point(x_index, y_index)) == enemy_agent_points.end() && enemy_conflict_points.find(procon::Point(x_index, y_index)) == enemy_conflict_points.end())
+                tile_scores.at(x_index).at(y_index) = std::max(field.getState(x_index, y_index).value, 0);
+            else
+                tile_scores.at(x_index).at(y_index) = -1e3;
+        }
+
+    // 相手の移動候補マスを除外、相手のいるマスを除外した上で、各エージェント毎にBFSをする
+    auto calc_one_agent = [&](int agent_index, int first_move){
+        int max_depth = std::min(this->max_depth, field.getTurn().final - field.getTurn().now) - 1;
+        const auto& start_point = field.getAgent(side, agent_index).getAppliedPosition(first_move);
+
+        if(field.outOfRangeCheck(start_point).first || enemy_agent_points.find(start_point) != enemy_agent_points.end() || enemy_conflict_points.find(start_point) != enemy_conflict_points.end())
+            return -1e9;
+        // SimpleBeamSearchのほぼ移植なので、気が向いたらコピペ実装を改善する ってずっと言ってる
+
+        using que_type = std::pair<double, std::vector<procon::Point>>;
+        std::priority_queue<que_type, std::vector<que_type>, std::greater<que_type>> now_que;
+        std::priority_queue<que_type, std::vector<que_type>, std::greater<que_type>> next_que;
+        now_que.emplace(tile_scores.at(start_point.x).at(start_point.y) * 2, std::vector<procon::Point>(1, start_point));
+
+        for(int depth = 0; depth < max_depth - 1; ++depth){
+            std::priority_queue<que_type, std::vector<que_type>, std::greater<que_type>> next_next_que;
+            while(!now_que.empty()){
+                auto [value, moves] = now_que.top();
+                value *= 1.4;
+                now_que.pop();
+
+                // 行動した回数
+                std::set<procon::Point> change_points;
+                for(auto& point : moves)
+                    change_points.insert(point);
+
+                const procon::Point& now_point = moves.back();
+                for(int move_index = 0; move_index < 9; ++move_index){
+                    procon::Point moved_point = now_point.getAppliedPosition(move_index);
+                    if(field.outOfRangeCheck(moved_point).first == true)
+                        continue;
+
+                    bool conflict_flag = (enemy_agent_points.find(moved_point) != enemy_agent_points.end() || enemy_conflict_points.find(moved_point) != enemy_conflict_points.end());
+
+                    if(conflict_flag)
+                        continue;
+
+                    auto new_moves = moves;
+                    new_moves.emplace_back(moved_point);
+
+                    bool already_moved = (change_points.find(moved_point) != change_points.end());
+                    auto state = field.getState(moved_point);
+                    auto tile_score = tile_scores.at(moved_point.x).at(moved_point.y);
+
+                    if(state.isEmpty() == true && already_moved == false)
+                        next_que.emplace(value + tile_score, new_moves);
+                    else if((state.isEmpty() == true && already_moved == true) || state.getDecrementedSide() == side)
+                        next_que.emplace(value, new_moves);
+                    else
+                        next_next_que.emplace(value + 2 * tile_score, new_moves);
+                }
+
+                while(static_cast<int>(next_que.size()) > max_width)
+                    next_que.pop();
+                while(static_cast<int>(next_next_que.size()) > max_width)
+                    next_next_que.pop();
+            }
+            now_que = std::move(next_que);
+            next_que = std::move(next_next_que);
+        }
+        return now_que.top().first;
+    };
+
+    std::vector<std::vector<double>> scores(agent_count, std::vector<double>(8));
+    for(int agent_index = 0; agent_index < agent_count; ++agent_index)
+        for(int first_move = 0; first_move < 8; ++first_move)
+            scores.at(agent_index).at(first_move) = calc_one_agent(agent_index, first_move);
+
+    // マッチング可能な候補に対して辺を張ると、適当に燃やす埋める問題に帰着することができる
+    // s側を「適当に動かした時の辺」、t側を「コンフリクト解消に
+
 }
--- Algorithms/testalgorithm.h
+++ Algorithms/testalgorithm.h
@@ -2,12 +2,22 @@
 #define TESTALGORITHM_H
 
 #include "algorithmwrapper.h"
+#include "simplebeamsearch.h"
 
 class TestAlgorithm : public AlgorithmWrapper
 {
+    // テスト用 諸々の動作確認をしたい時に使うアルゴリズム
+
     using AlgorithmWrapper::AlgorithmWrapper;
 public:
     std::vector<procon::MoveState> agentAct() override;
+
+private:
+    // SimpleBeamSearchベース、コンフリクトを発生させられる場合に優先的にそれをします
+    std::vector<procon::MoveState> testMakeConflict();
+
+    const int max_depth = 10;
+    const int max_width = 30;
 };
 
 #endif // TESTALGORITHM_H
