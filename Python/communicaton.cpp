ccd9e704bb525fe1785e1ed7ecd900858e6f0435
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-04-11 12:27:54 +0900

    [add]add getScore to BOOST_PYTHON_MODULE

--- Python/communicaton.cpp
+++ Python/communicaton.cpp
@@ -40,6 +40,7 @@ BOOST_PYTHON_MODULE(communication){
     // procon::Fieldをpython側で用意せずに、numpy行列4つ(赤青のタイル状況、エージェント状況をboolにしたもの)を送る事にする
     // 必要になったら適宜用意する
     bp::class_<Board>("Board").def("getData", &Board::getData);
+    bp::class_<Board>("Board").def("getScore", &Board::getScore);
 }
 
 }
