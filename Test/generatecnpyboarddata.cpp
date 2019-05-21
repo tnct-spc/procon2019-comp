e3c03a95942e7264347b49e206d97ccdcbe6c84f
Hiroki Shibata <s17097@tokyo.kosen-ac.jp>
2019-05-21 18:19:11 +0900

    [update]npz_save

--- Test/generatecnpyboarddata.cpp
+++ Test/generatecnpyboarddata.cpp
@@ -18,6 +18,13 @@ void GenerateCnpyBoardData::run(){
     */
 
     for(int _ = 0; _ < playout_count; ++_, [&]{
+
+        std::string save_filename = save_dir + "/" + std::to_string(procon::random::call()) + ".npz";
+        std::vector<std::vector<float>> field_vectors(2);
+        std::vector<std::vector<float>> policy_vectors(2);
+
+        const Py_intptr_t* shape_tuple;
+
         procon::communication::Board board(csv_path);
         std::vector<std::pair<procon::communication::Board, std::vector<np::ndarray>>> ret_data;
         auto& field = board.field;
@@ -57,29 +64,38 @@ void GenerateCnpyBoardData::run(){
             for(auto& pol : policy)
                 pol[pol.get_shape()[0] - 1] *= result;
 
+            // TODO: procon::npy::cnpyExportと同じ形式を使う(cnpyExport自体はちゃんとした形式で出してくれないので, どうにかする)
             auto field_ndarr = board.getData();
             auto field_ndarr_rev = board.reverseBoard().getData();
 
-            auto shape_tuple = field_ndarr.get_shape();
-            std::vector<std::vector<std::vector<std::vector<float>>>> field_vector(2, std::vector<std::vector<std::vector<float>>>(shape_tuple[0], std::vector<std::vector<float>>(shape_tuple[1], std::vector<float>(shape_tuple[2]))));
-            for(int x = shape_tuple[0]; x < shape_tuple[0]; ++x)
-                for(int y = shape_tuple[1]; y < shape_tuple[1]; ++y)
-                    for(int z = shape_tuple[2]; z < shape_tuple[2]; ++z){
-                        field_vector[0][x][y][z] = bp::extract<float>(field_ndarr[x][y][z]);
-                        field_vector[1][x][y][z] = bp::extract<float>(field_ndarr_rev[x][y][z]);
+            shape_tuple = field_ndarr.get_shape();
+            int arr_size = policy[0].get_shape()[0];
+            std::vector<std::vector<float>> field_vector(2);
+            std::vector<std::vector<float>> policy_vector(2, std::vector<float>(arr_size));
+            for(int x = 0; x < shape_tuple[0]; ++x)
+                for(int y = 0; y < shape_tuple[1]; ++y)
+                    for(int z = 0; z < shape_tuple[2]; ++z){
+                        field_vector[0].emplace_back(bp::extract<int>(field_ndarr[x][y][z]));
+                        field_vector[1].emplace_back(bp::extract<int>(field_ndarr_rev[x][y][z]));
                     }
 
-            int arr_size = policy[0].get_shape()[0];
-            std::vector<std::vector<float>> policy_vector(2, std::vector<float>(arr_size));
             for(int side = 0; side < 2; ++side)
                 for(int index = 0; index < arr_size; ++index)
                     policy_vector[side][index] = bp::extract<float>(policy[side][index]);
 
             for(int side = 0; side < 2; ++side){
-                std::string save_filename = save_dir + "/" + std::to_string(procon::random::call()) + ".npz";
-                cnpy::npz_save(save_filename, "input", &field_vector[side][0][0][0], std::vector<size_t>{static_cast<size_t>(shape_tuple[0]), static_cast<size_t>(shape_tuple[1]), static_cast<size_t>(shape_tuple[2])}, "w");
-                cnpy::npz_save(save_filename, "output", policy_vector[side], "a");
+                for(auto& elm : field_vector[side])
+                    field_vectors[side].emplace_back(elm);
+                for(auto& elm : policy_vector[side])
+                    policy_vectors[side].emplace_back(elm);
             }
+        }
+        assert(shape_tuple != nullptr);
+        cnpy::npz_save(save_filename, "input_shape", std::vector<int>{static_cast<int>(shape_tuple[0]), static_cast<int>(shape_tuple[1]), static_cast<int>(shape_tuple[2])}, "w");
+        cnpy::npz_save(save_filename, "output_shape", std::vector<int>{static_cast<int>(policy_vectors[0].size())}, "a");
+        for(int side = 0; side < 2; ++side){
+            cnpy::npz_save(save_filename, "input_" + std::to_string(side), field_vectors[side], "a");
+            cnpy::npz_save(save_filename, "output_" + std::to_string(side), policy_vectors[side], "a");
         }
     }());
 }
