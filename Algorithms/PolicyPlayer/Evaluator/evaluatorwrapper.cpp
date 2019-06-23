34654d1c8044c8ad6ad047cce79a3fdd44377c9e
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-06-24 08:24:08 +0900

    [add]calcInsidePolicy

--- Algorithms/PolicyPlayer/Evaluator/evaluatorwrapper.cpp
+++ Algorithms/PolicyPlayer/Evaluator/evaluatorwrapper.cpp
@@ -4,3 +4,17 @@ EvaluatorWrapper::EvaluatorWrapper()
 {
 
 }
+
+boost::python::numpy::ndarray EvaluatorWrapper::calcPolicy(const procon::Field& field){
+    auto result = calcInsidePolicy(field);
+    auto shape = result.get_shape();
+    for(int agent = 0; agent < shape[0]; ++agent){
+        float sum = 0;
+        for(int move = 0; move < shape[1]; ++move)
+            sum += boost::python::extract<float>(result[agent][move]);
+        assert(sum != 0.0);
+        for(int move = 0; move < shape[1]; ++move)
+            result[agent][move] = boost::python::extract<float>(result[agent][move]) / sum;
+    }
+    return result;
+}
--- Algorithms/PolicyPlayer/Evaluator/evaluatorwrapper.h
+++ Algorithms/PolicyPlayer/Evaluator/evaluatorwrapper.h
@@ -9,7 +9,10 @@ class EvaluatorWrapper
 {
 public:
     EvaluatorWrapper();
-    virtual boost::python::numpy::ndarray calcPolicy(const procon::Field& field) = 0;
+    virtual boost::python::numpy::ndarray calcPolicy(const procon::Field& field);
+
+protected:
+    virtual boost::python::numpy::ndarray calcInsidePolicy(const procon::Field& field) = 0;
 };
 
 #endif // EVALUATORWRAPPER_H
