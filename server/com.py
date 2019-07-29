9822f05ce951886321a74166dc83a92773687777
okdwtr <s17033@tokyo.kosen-ac.jp>
2019-07-29 21:03:35 +0900

    [add] add communication to server
    バグありだが取り急ぎ

---
+++ server/com.py
@@ -0,0 +1,73 @@
+# encording : utf-8
+""" get and post json data for procon30 comp server """
+
+#default_token = "procon30_example_token"
+
+import sys, urllib.request, json
+
+args = sys.argv
+json_def = {"actions": [{"agentID": 0, "dx": 0, "dy": 0, "type": "move"}]}
+
+def checkConnection(ip, port, token = "procon30_example_token"):
+    """  check connection for procon30 comp server """
+
+    url = "http://" + str(ip) + ":" + str(port) + "/ping"
+
+    headers = {
+        "Authorization": token
+    }
+
+    content = urllib.request.urlopen(url = url, headers=headers)
+
+    return json.loads(content)
+
+
+def getMatches(ip, port, token = "procon30_example_token"):
+    """ get matches json data from procon30 comp server """
+
+    url = "http://" + str(ip) + ":" + str(port) + "/matches"
+
+    headers = {
+        "Authorization": token
+    }
+
+    content = urllib.request.urlopen(url = url, headers=headers)
+
+    return json.loads(content)
+
+
+def getMatchStatus(ip, port, token = "procon30_example_token", matchid = 6):
+    """ get match status json data from procon30 comp server """
+
+    url = "http://" + str(ip) + ":" + str(port) + "/matches" + str(matchid)
+
+    headers = {
+        "Authorization": token
+    }
+
+    content = urllib.request.urlopen(url = url, headers=headers)
+
+    return json.loads(content)
+
+
+def sendAction(ip, port, token = "procon30_example_token", matchid = 1, json = json_def):
+    """ post action json data to procon30 comp server """
+
+    url = "http://" + str(ip) + ":" + str(port) + "/matches" + str(matchid) + "/action"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": token
+    }
+
+    req = urllib.request.Request(url=url, headers=headers, data=json)
+    f = urllib.request.urlopen(req)
+    return f.read().decode('utf-8')
+
+if __name__ == "__main__":
+    if len(args) == 2:
+        print(checkConnection(args[1], args[2]), args[3])
+    elif len(args) == 3:
+        print(checkConnection(args[1], args[2]), args[3])
+    else:
+        pass
