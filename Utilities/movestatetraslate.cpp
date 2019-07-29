307bdb9479e5872dd0aa6ae2baeebc4676ad1be2
Your name <you@example.com>
2019-07-29 11:19:47 +0900

    [add]file

---
+++ Utilities/movestatetraslate.cpp
@@ -0,0 +1,8 @@
+#include "movestatetraslate.h"
+
+std::vector<procon::MoveState> procon::json::translateToMoveStateData(std::string json_str){
+
+}
+std::string procon::json::translateFromMoveStateData(std::vector<procon::MoveState>){
+
+}
---
+++ Utilities/movestatetraslate.h
@@ -0,0 +1,11 @@
+#ifndef MOVESTATETRASLATE_H
+#define MOVESTATETRASLATE_H
+
+
+class MoveStateTraslate
+{
+public:
+    MoveStateTraslate();
+};
+
+#endif // MOVESTATETRASLATE_H
