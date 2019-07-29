fe79a7205befb479570313e0118d2bc7e8679475
spc <s19115@tokyo.kosen-ac.jp>
2019-07-29 15:23:45 +0900

    [add]

--- Utilities/fieldcsvtranslate.cpp
+++ Utilities/fieldcsvtranslate.cpp
@@ -6,3 +6,11 @@ std::string procon::json::translateToFieldCsv(std::string json_str){
 std::string procon::json::translateFromFieldCsv(std::string field_csv_str){
 
 }
+
+procon::Field procon::json::importJsonToField(std::string path){
+    // pathに存在しているファイルを読み込む
+
+    // ファイルの中身をtranslateFromFieldCsvに投げて, 結果を取得する
+
+    // 出てきたcsv形式の文字列をprocon::Field形式に変換して, returnする
+}
