c69876e94741f82992fcfdebe1147852f78099ab
Your name <you@example.com>
2019-07-29 12:06:54 +0900

    [add]関数の実装

--- Utilities/movestatetraslate.cpp
+++ Utilities/movestatetraslate.cpp
@@ -5,4 +5,27 @@ std::vector<procon::MoveState> procon::json::translateToMoveStateData(std::strin
 }
 std::string procon::json::translateFromMoveStateData(std::vector<procon::MoveState>){
 
+
+}
+
+std::tuple<std::string, int, int> procon::json::convertFromMoveState(procon::MoveState move_state){
+
+    procon::Point point = move_state.getMove();
+    bool is_delete = move_state.isDelete();
+    int x = point.x;
+    int y = point.y;
+
+    std::string type;
+    int dx, dy;
+
+    type = "move";
+    dx = x;
+    dy = y;
+
+    if (is_delete == true) {
+        type = "remove";
+    }
+
+    // make_tuple
+    return std::make_tuple(type, dx, dy);
 }
