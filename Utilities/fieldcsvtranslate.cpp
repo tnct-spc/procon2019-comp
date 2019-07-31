3b5343353cb25c5497b0d72efb0f87feda5054c1
spc <s19115@tokyo.kosen-ac.jp>
2019-07-31 10:23:37 +0900

    [add]some

--- Utilities/fieldcsvtranslate.cpp
+++ Utilities/fieldcsvtranslate.cpp
@@ -8,9 +8,21 @@ std::string procon::json::translateFromFieldCsv(std::string field_csv_str){
 }
 
 procon::Field procon::json::importJsonToField(std::string path){
-    // pathに存在しているファイルを読み込む
+
+}
+
+int main()
+{
+
+        std::ifstream ifs("フィールド情報_turn0.json");
+        std::string str;
+        std::istreambuf_iterator<char> it(ifs);
+            std::istreambuf_iterator<char> last;
+            std::string str(it, last);
+       procon::json::translateFromFieldCsv(str);// ファイルの中身をtranslateFromFieldCsvに投げて, 結果を取得する
+}
 
     // ファイルの中身をtranslateFromFieldCsvに投げて, 結果を取得する
 
     // 出てきたcsv形式の文字列をprocon::Field形式に変換して, returnする
-}
+
--- Utilities/fieldcsvtranslate.h
+++ Utilities/fieldcsvtranslate.h
@@ -2,10 +2,17 @@
 #define FIELDCSVTRANSLATE_H
 
 #include "field.h"
+#include <iostream>// pathに存在しているファイルを読み込む
+#include <fstream>
+#include <string>
+#include <stdio.h>
+
 
 namespace procon::json{
     std::string translateToFieldCsv(std::string json_str);
     std::string translateFromFieldCsv(std::string field_csv_str);
+
+    procon::Field importJsonToField(std::string path);
 }
 
 #endif // FIELDCSVTRANSLATE_H
