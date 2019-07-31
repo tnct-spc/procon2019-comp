885bdcae4ad720be35159c1ae8381b976e70208f
Your name <you@example.com>
2019-07-31 10:24:56 +0900

    [add]header

--- Utilities/movestatetraslate.h
+++ Utilities/movestatetraslate.h
@@ -1,11 +1,12 @@
 #ifndef MOVESTATETRASLATE_H
 #define MOVESTATETRASLATE_H
 
+#include "field.h"
 
-class MoveStateTraslate
-{
-public:
-    MoveStateTraslate();
+namespace procon::json{
+    std::vector<procon::MoveState> translateToMoveStateData(std::string json_str);
+    std::string std::tuple<std::string, int, int> convertFromMoveState(procon::MoveState);translateFromMoveStateData(std::vector<procon::MoveState>);
+    std::tuple<std::string, int, int> convertFromMoveState(procon::MoveState);
 };
 
 #endif // MOVESTATETRASLATE_H
