56432030bcd93809525edb0b54dc57b4b04800e8
Shuta Kato <s19045@tokyo.kosen-ac.jp>
2019-08-05 15:01:56 +0900

    [add]機能追加

--- Utilities/buttleapi.h
+++ Utilities/buttleapi.h
@@ -2,6 +2,8 @@
 #define BUTTLEAPI_H
 
 #include "field.h"
+#include "csvtranslate.h"
+#include "movestatetraslate.h"
 
 namespace procon{
 
@@ -11,7 +13,7 @@ public:
     ButtleAPI(std::string host, int port, std::string token);
     std::vector<std::pair<int,int>> getMatches();
     procon::Field getField(int match_id);
-    void setMove(int match_id, std::vector<procon::MoveState> moves);
+    void setMove(int match_id, std::vector<procon::MoveState> moves, std::vector<int> agent_ids);
 };
 
 }
